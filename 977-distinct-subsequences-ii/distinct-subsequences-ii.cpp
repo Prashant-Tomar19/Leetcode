c628585491c1ba48dbd4f6dbf81c9566cc822eb9
Prashant-Tomar19 <prashanttomar7581@gmail.com>
2026-07-07 16:14:30 +0530

    Time: 2 ms (39.51%) | Memory: 8.9 MB (82.98%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -8,22 +8,17 @@ public:
         int freq[26] = {0};
 
         for(int i = 0; i < s.size(); i++) {
-            if(freq[s[i]-'a'] == 0) {
+            
                 if(i == 0) {
                     total = 1;
                     freq[s[i]-'a'] = 1;
                 }
                 else {
-                    neww = (total + 1) % MOD;
-                    total = (total + neww) % MOD;
-                    freq[s[i]-'a'] = neww;
-                }
-            }
-            else {
                 neww = ( (total - freq[s[i]-'a'] + 1) % MOD + MOD ) % MOD;
                 total = (total + neww) % MOD;
                 freq[s[i]-'a'] = (freq[s[i]-'a'] + neww) % MOD;
             }
+            
         }
 
         return total;
